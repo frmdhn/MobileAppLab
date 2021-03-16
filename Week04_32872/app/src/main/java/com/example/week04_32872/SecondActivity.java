615cb0b0b2a6b3c626fc967632014de6135911da
frmdhn <64734864+frmdhn@users.noreply.github.com>
2021-03-16 20:55:48 +0700

    Tugas Mobile App Lab

---
+++ Week04_32872/app/src/main/java/com/example/week04_32872/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.week04_32872;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        setTitle("Week04_32872);
+    }
+}
